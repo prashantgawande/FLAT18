29634c33b74b83952cd99c9c41f66defcd48b306
prashantgawande <prashantngawande26@gmail.com>
2018-03-21 20:53:54 +0530

    course file

---
+++ Course.java
@@ -0,0 +1,33 @@
+package many2many_crita;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cou1_tab")
+public class Course {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private int cid;
+	@Column
+	private String cname;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+}
